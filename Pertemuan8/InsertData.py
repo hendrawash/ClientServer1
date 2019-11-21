fbc611682913c1426cfceffc2f7064bdf12bebab
PtspDev <PtspDev@DESKTOP-3BCU4OH>
2019-11-21 08:28:51 +0700

    pertemuan8, mysql connection

---
+++ Pertemuan8/InsertData.py
@@ -0,0 +1,12 @@
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", user="root", passwd="hendrawan", db="kampus")
+c = db.cursor()
+sql = "INSERT INTO Mahasiswa(NIM,Nama,Alamat) VALUES (%s, %s, %s)"
+val = ("001", "Hendrawan", "Brebes")
+c.execute(sql, val)
+
+#commit untuk apply perubahan data
+db.commit()
+
+c.close()
---
+++ Pertemuan8/connect.py
@@ -0,0 +1,10 @@
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", user="root", passwd="hendrawan", db="kampus")
+c = db.cursor()
+c.execute("select version()")
+rows = c.fetchall()
+for row in rows:
+    print("MySQL Version %s" % row[0])
+
+c.close()
