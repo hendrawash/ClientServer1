461cbb63b27cc2f0aa62de62009982471f8ee39d
PtspDev <PtspDev@DESKTOP-3BCU4OH>
2019-12-05 09:12:26 +0700

    add database query bener

--- Pertemuan10/SegiTiga/Luas.py
+++ Pertemuan10/SegiTiga/Luas.py
@@ -1,7 +1,8 @@
-class SegiTiga:
+
+class Luas:
     def __init__(self, alas, tinggi):
         self.__alas = alas
         self.__tinggi = tinggi
 
-    def Luas(self):
+    def Hitung(self):
         return (self.__alas * self.__tinggi) / 2
---
+++ Pertemuan10/main_func.py
@@ -0,0 +1,5 @@
+import Pertemuan10.SegiTiga.Luas as Luas
+
+if __name__ == '__main__':
+    segiTiga = Luas.Luas(10, 5)
+    print(segiTiga.Hitung())
